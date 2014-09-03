902d64c04997534e9f8a8b3bae6c48379cc0a478
Joël Cox <joel@joelcox.nl>
2014-09-03 14:28:04 +0200

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(
+    name='matchbox',
+    version='0.1.0',
+    author='Joël Cox',
+    author_email='joel@joelcox.nl',
+    packages=['matchbox'],
+    scripts=[],
+    url='https://github.com/joelcox/matchbox',
+    license='MIT',
+    description='Experimental Python tools.',
+    long_description=open('README.md').read()
+)
